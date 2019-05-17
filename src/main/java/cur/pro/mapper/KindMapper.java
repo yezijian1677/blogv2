6640e8640c6739376575150b4160846379988aa0
yezijian <1677390657@qq.com>
2019-05-17 14:15:31 +0800

    add some mapper

--- src/main/java/cur/pro/mapper/KindMapper.java
+++ src/main/java/cur/pro/mapper/KindMapper.java
@@ -1,6 +1,9 @@
 package cur.pro.mapper;
 
 import cur.pro.entity.Kind;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface KindMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +17,12 @@ public interface KindMapper {
     int updateByPrimaryKeySelective(Kind record);
 
     int updateByPrimaryKey(Kind record);
+
+    List<Kind> selectAll();
+
+    List<Kind> selectByIds(@Param(value = "ids") List<Integer> ids);
+
+    Kind selectByName(String name);
+
+    List<Integer> selectIdByLikeName(String name);
 }
