5468f6df68bb683580e0824631639ea18095bf94
yezijian <1677390657@qq.com>
2019-05-17 14:40:07 +0800

    add some mapper

--- src/main/java/cur/pro/mapper/KindMapperMapper.java
+++ src/main/java/cur/pro/mapper/KindMapperMapper.java
@@ -1,9 +1,28 @@
 package cur.pro.mapper;
 
 import cur.pro.entity.KindMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface KindMapperMapper {
     int insert(KindMapper record);
 
     int insertSelective(KindMapper record);
+
+    int insertKindBatch(@Param(value = "articleId") Integer articleId,
+                        @Param(value = "kinds") List<Integer> kinds);
+
+    int insertArticleBatch(@Param(value = "kind") Integer kind,
+                           @Param(value = "articleIds") List<Integer> articleIds);
+
+    List<Integer> selectArticleByKind(Integer kind);
+
+    List<Integer> selectKindByArticleId(Integer articleId);
+
+    List<Integer> selectBatchByKinds(@Param(value = "kinds") List<Integer> kinds);
+
+    int deleteByArticleId(Integer articleId);
+
+    int deleteByKind(Integer kind);
 }
